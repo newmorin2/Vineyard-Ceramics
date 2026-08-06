8ca6abc49350acc31db5e19980578be67b56feaa
newmorin2 <newton.mwangi@student.moringaschool.com>
2026-08-06 16:24:48 +0300

    mkaing navbar responsive

---
+++ index.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const hamburger = document.getElementById("hamburger");
+    const navLinks = document.getElementById("nav-links");
+    hamburger.addEventListener("click", () => {
+        navLinks.classList.toggle("active");
+    })
+});
